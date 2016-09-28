e806fb377f8bc57aff64921b72f491d5e4bc1e95
tmpUsername <admin@fddf-PC>
2016-09-28 17:00:09 +0200

    lalalalalalalalalal

---
+++ src/java/streaming/servlet/ListerSeriesServlet.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package streaming.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import streaming.service.SerieService;
+
+/**
+ *
+ * @author admin
+ */
+@WebServlet(name = "ListerSeriesServlet", urlPatterns = {"/liste_series"})
+public class ListerSeriesServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("mesSeries", new SerieService().liste());
+        req.getRequestDispatcher("lister_series.jsp").forward(req, resp);
+    }
+
+}
