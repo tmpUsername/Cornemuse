cdf04ca40dd60b4737c9e69e4bca6664643c395c
tmpUsername <admin@fddf-PC>
2016-10-04 09:40:10 +0200

    ajout de classes de test unitaire

---
+++ test/streaming/service/test/FilmServiceTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package streaming.service.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import streaming.entity.Film;
+
+/**
+ *
+ * @author admin
+ */
+public class FilmServiceTest {
+    
+    
+    
+}
