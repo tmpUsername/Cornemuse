9b30801dd59621f4799775fd2fc807f6192d8ddd
tmpUsername <admin@fddf-PC>
2016-10-04 13:51:08 +0200

    ajout des tests pour genre et pays

--- test/streaming/service/test/GenreServiceTest.java
+++ test/streaming/service/test/GenreServiceTest.java
@@ -5,16 +5,95 @@
  */
 package streaming.service.test;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import junit.framework.Assert;
+import org.junit.AfterClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import streaming.entity.Genre;
+import streaming.service.GenreService;
 
 /**
  *
  * @author admin
  */
 public class GenreServiceTest {
-    
-    public GenreServiceTest() {
+
+    @BeforeClass
+    public static void initEtList() {
+        EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+
+        em.getTransaction().begin();
+
+        em.createQuery("DELETE FROM Genre g WHERE g.id=" + 6L).executeUpdate();
+        em.createQuery("DELETE FROM Genre g WHERE g.id=" + 7L).executeUpdate();
+        em.createQuery("DELETE FROM Genre g WHERE g.id=" + 8L).executeUpdate();
+
+        em.getTransaction().commit();
+
+        //AJOUT
+        em.getTransaction().begin();
+
+        Genre aSuppr = new Genre();
+        Genre aModifEtFind = new Genre();
+        aSuppr.setId(7L);
+        aModifEtFind.setId(8L);
+
+        em.persist(aSuppr);
+        em.persist(aModifEtFind);
+
+        em.getTransaction().commit();
+
+        //test Lister
+        Assert.assertEquals(7, new GenreService().lister().size());
     }
-    
+
+    @AfterClass
+    public static void clean() {
+        EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+
+        em.getTransaction().begin();
+
+        em.createQuery("DELETE FROM Genre g WHERE g.id=" + 6L).executeUpdate();
+        em.createQuery("DELETE FROM Genre g WHERE g.id=" + 7L).executeUpdate();
+        em.createQuery("DELETE FROM Genre g WHERE g.id=" + 8L).executeUpdate();
+
+        em.getTransaction().commit();
+    }
+
+    @Test
+    public void ajouterOK() {
+        Genre g = new Genre();
+        g.setId(6L);
+        g.setNom("LALALA");
+
+        new GenreService().ajouter(g);
+
+        Assert.assertEquals((Long) 6L, new GenreService().findByID(6L).getId());
+    }
+
+    @Test
+    public void modifierOK() {
+        Genre g = new GenreService().findByID(8L);
+
+        g.setNom("Canard");
+
+        new GenreService().modifer(g);
+
+        Assert.assertEquals("Canard", new GenreService().findByID(8L).getNom());
+    }
+
+    @Test
+    public void supprimerOK() {
+        new GenreService().supprimer(7L);
+
+        Assert.assertNull(new GenreService().findByID(7L));
+    }
+
+    @Test
+    public void findByID() {
+        Assert.assertNotNull(new GenreService().findByID(8L));
+    }
+
 }
--- test/streaming/service/test/PaysServiceTest.java
+++ test/streaming/service/test/PaysServiceTest.java
@@ -5,16 +5,95 @@
  */
 package streaming.service.test;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import junit.framework.Assert;
+import org.junit.AfterClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import streaming.entity.Pays;
+import streaming.service.PaysService;
 
 /**
  *
  * @author admin
  */
 public class PaysServiceTest {
-    
-    public PaysServiceTest() {
+
+    @BeforeClass
+    public static void initEtList() {
+        EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+
+        em.getTransaction().begin();
+
+        em.createQuery("DELETE FROM Pays p WHERE p.id=" + 4L).executeUpdate();
+        em.createQuery("DELETE FROM Pays p WHERE p.id=" + 5L).executeUpdate();
+        em.createQuery("DELETE FROM Pays p WHERE p.id=" + 6L).executeUpdate();
+
+        em.getTransaction().commit();
+
+        //AJOUT
+        em.getTransaction().begin();
+
+        Pays aSuppr = new Pays();
+        Pays aModifEtFind = new Pays();
+        aSuppr.setId(5L);
+        aModifEtFind.setId(6L);
+
+        em.persist(aSuppr);
+        em.persist(aModifEtFind);
+
+        em.getTransaction().commit();
+
+        //test Lister
+        Assert.assertEquals(5, new PaysService().lister().size());
     }
-    
+
+    @AfterClass
+    public static void clean() {
+        EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+
+        em.getTransaction().begin();
+
+        em.createQuery("DELETE FROM Pays p WHERE p.id=" + 4L).executeUpdate();
+        em.createQuery("DELETE FROM Pays p WHERE p.id=" + 5L).executeUpdate();
+        em.createQuery("DELETE FROM Pays p WHERE p.id=" + 6L).executeUpdate();
+
+        em.getTransaction().commit();
+    }
+
+    @Test
+    public void ajouterOK() {
+        Pays p = new Pays();
+        p.setId(4L);
+        p.setNom("LALALA");
+
+        new PaysService().ajouter(p);
+
+        Assert.assertEquals((Long) 4L, new PaysService().findByID(4L).getId());
+    }
+
+    @Test
+    public void modifierOK() {
+        Pays p = new PaysService().findByID(6L);
+
+        p.setNom("Canard");
+
+        new PaysService().modifer(p);
+
+        Assert.assertEquals("Canard", new PaysService().findByID(6L).getNom());
+    }
+
+    @Test
+    public void supprimerOK() {
+        new PaysService().supprimer(5L);
+
+        Assert.assertNull(new PaysService().findByID(5L));
+    }
+
+    @Test
+    public void findByID() {
+        Assert.assertNotNull(new PaysService().findByID(6L));
+    }
+
 }
