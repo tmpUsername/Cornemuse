237ace865dd493093ff185c7c7e5c9e55e498d47
tmpUsername <admin@fddf-PC>
2016-10-04 12:01:58 +0200

    test service film et serie

--- test/streaming/service/test/FilmServiceTest.java
+++ test/streaming/service/test/FilmServiceTest.java
@@ -5,16 +5,101 @@
  */
 package streaming.service.test;
 
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import junit.framework.Assert;
+import org.junit.AfterClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import streaming.entity.Film;
+import streaming.service.FilmService;
 
 /**
  *
  * @author admin
  */
 public class FilmServiceTest {
-    
-    
-    
+
+    @BeforeClass
+    public static void init() {
+        EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+        //Clean
+        em.getTransaction().begin();
+        em.createQuery("DELETE FROM Film f WHERE f.id=" + 7L).executeUpdate();
+        em.createQuery("DELETE FROM Film f WHERE f.id=" + 8L).executeUpdate();
+        em.createQuery("DELETE FROM Film f WHERE f.id=" + 9L).executeUpdate();
+        em.getTransaction().commit();
+
+        //DEBUT Ajout
+        em.getTransaction().begin();
+
+        Film aSuppr = new Film(8L, "lalala", "kakakak", 1234, 90);
+        Film aTrouvEtMod = new Film(9L, "lalala", "kakakak", 1234, 90);
+
+        em.persist(aSuppr);
+        em.persist(aTrouvEtMod);
+
+        em.getTransaction().commit();
+        //FIN Ajout
+        
+        List<Film> f = new FilmService().lister();
+        Assert.assertEquals(6, f.size());
+    }
+
+    @AfterClass
+    public static void clean(){
+        EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+        
+        em.getTransaction().begin();
+        em.createQuery("DELETE FROM Film f WHERE f.id=" + 7L).executeUpdate();
+        em.createQuery("DELETE FROM Film f WHERE f.id=" + 8L).executeUpdate();
+        em.createQuery("DELETE FROM Film f WHERE f.id=" + 9L).executeUpdate();
+        em.getTransaction().commit();
+    }
+
+    @Test
+    public void ajouterOK() {
+        Film f = new Film();
+        f.setId(7L);
+        f.setDuree(90);
+        f.setAnnee(1980);
+        f.setTitre("lalala");
+
+        new FilmService().ajouter(f);
+
+        assertEquals((Long) 7L, new FilmService().findByID(7L).getId());
+
+    }
+
+    @Test
+    public void supprimer() {
+
+        new FilmService().supprimer(8L);
+
+        Assert.assertNull(new FilmService().findByID(8L));
+
+    }
+
+    @Test
+    public void modifier() {
+
+        Film f = new FilmService().findByID(9L);
+
+        f.setTitre("P'tit Beur'");
+
+        new FilmService().modifier(f);
+
+        f = new FilmService().findByID(9L);
+
+        Assert.assertEquals("P'tit Beur'", f.getTitre());
+    }
+
+    @Test
+    public void find() {
+        Assert.assertNotNull(new FilmService().findByID(9L));
+    }
+
 }
--- test/streaming/service/test/SerieServiceTest.java
+++ test/streaming/service/test/SerieServiceTest.java
@@ -5,16 +5,95 @@
  */
 package streaming.service.test;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import junit.framework.Assert;
+import org.junit.AfterClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import streaming.entity.Serie;
+import streaming.service.SerieService;
 
 /**
  *
  * @author admin
  */
 public class SerieServiceTest {
-    
-    public SerieServiceTest() {
+
+    @BeforeClass
+    public static void initEtList() {
+        EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+
+        em.getTransaction().begin();
+
+        em.createQuery("DELETE FROM Serie s WHERE s.id=" + 3L).executeUpdate();
+        em.createQuery("DELETE FROM Serie s WHERE s.id=" + 4L).executeUpdate();
+        em.createQuery("DELETE FROM Serie s WHERE s.id=" + 5L).executeUpdate();
+
+        em.getTransaction().commit();
+        
+        //AJOUT
+        em.getTransaction().begin();
+        
+        Serie aSuppr = new Serie();
+        Serie aModifEtFind   = new Serie();
+        aSuppr.setId(4L);
+        aModifEtFind.setId(5L);
+        
+        em.persist(aSuppr);
+        em.persist(aModifEtFind);
+        
+        em.getTransaction().commit();
+        
+        //test Lister
+        Assert.assertEquals(4, new SerieService().liste().size());
+    }
+
+    @AfterClass
+    public static void clean() {
+        EntityManager em = Persistence.createEntityManagerFactory("PU").createEntityManager();
+
+        em.getTransaction().begin();
+
+        em.createQuery("DELETE FROM Serie s WHERE s.id=" + 3L).executeUpdate();
+        em.createQuery("DELETE FROM Serie s WHERE s.id=" + 4L).executeUpdate();
+        em.createQuery("DELETE FROM Serie s WHERE s.id=" + 5L).executeUpdate();
+
+        em.getTransaction().commit();
     }
     
+    @Test
+    public void ajouterOK(){
+        Serie s = new Serie();
+        s.setId(3L);
+        s.setTitre("lalala");
+        
+        new SerieService().ajouter(s);
+        
+        Assert.assertEquals((Long) 3L, new SerieService().findByID(3L).getId());
+    }
+    
+    @Test
+    public void modifierOK(){
+        Serie s = new SerieService().findByID(5L);
+        
+        s.setTitre("Canard");
+        
+        new SerieService().modifier(s);
+        
+        Assert.assertEquals("Canard", new SerieService().findByID(5L).getTitre());
+    }
+    
+    @Test
+    public void supprimerOK(){
+        new SerieService().supprimer(4L);
+        
+        Assert.assertNull(new SerieService().findByID(4L));
+    }
+    
+    @Test
+    public void findByID(){
+        Assert.assertNotNull(new SerieService().findByID(5L));
+    }
 }
